1c385e622e944199063dfe49e1f1c798b6a47439
hector3211 <hectororopesa5@gmail.com>
2023-02-11 20:00:29 -0500

    changed .build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-cargo build --release
+ RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev
 cargo install diesel_cli --no-default-features --features postgres
+cargo build --release
