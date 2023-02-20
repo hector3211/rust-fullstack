57c36e53589e3115747a2fd28243bd6c9a6616fa
hector3211 <hectororopesa5@gmail.com>
2023-02-19 19:26:05 -0500

    [added]: Loading Suspense page

---
+++ client/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="animate-pulse pt-10">
+      <div className="bg-gray-600 min-h-fit min-w-fit mx-5 rounded-md">
+        Loading...
+      </div>
+    </div>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
 import AllTheMovies from "./AllMovies";
 import Footer from "./Footer";
+import Loading from "./loading";
 
 export default async function Home() {
   return (
     <main>
       <div className="pt-10">
-        {/* @ts-expect-error Server Component */}
-        <AllTheMovies />
+        <Suspense fallback={<Loading />}>
+          {/* @ts-expect-error Server Component */}
+          <AllTheMovies />
+        </Suspense>
       </div>
       <Footer />
     </main>
