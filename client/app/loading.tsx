45680ccb2e71d12c68060bde18e0955146f092fa
hector3211 <hectororopesa5@gmail.com>
2023-03-10 23:12:49 -0500

    added: radial progress with spin instead of loading...

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,8 +1,15 @@
 export default function Loading() {
   return (
-    <div className="animate-pulse pt-10">
-      <div className="bg-gray-600 min-h-fit min-w-fit mx-5 rounded-md">
-        Loading...
+    <div className="animate-pulse min-h-screen max-w-full">
+      <div className="h-screen w-full flex justify-center items-center mx-5 rounded-md">
+        <div className="flex flex-col justify-center items-center">
+          <div
+            className="radial-progress animate-spin"
+            // @ts-ignore
+            style={{ "--value": 25 }}
+          ></div>
+          <p>Refresh a couple times!</p>
+        </div>
       </div>
     </div>
   );
